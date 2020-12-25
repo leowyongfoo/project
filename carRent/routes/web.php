d697f7f34a21f1a1ba65d39094ce202e70e6dd05
leowyongfoo <jacksonleow6@gmail.com>
2020-12-25 15:17:40 +0800

    update route remark

--- carRent/routes/web.php
+++ carRent/routes/web.php
@@ -16,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//Tpye route
 Route::get('/insertType', [App\Http\Controllers\TypeController::class, 'create'])->name('addType');
 Route::post('/insertType/store', [App\Http\Controllers\TypeController::class, 'store'])->name('addType');
 Route::get('/viewType', [App\Http\Controllers\TypeController::class, 'view'])->name('showType');
 Route::get('/deleteType/{id}', [App\Http\Controllers\TypeController::class, 'delete'])->name('deleteType');
 
+//Car product route
 Route::get('/insertCar', [App\Http\Controllers\CarController::class, 'create'])->name('addCar');
 Route::post('/insertCar/store', [App\Http\Controllers\CarController::class, 'store'])->name('addCar');
 Route::get('/viewCar', [App\Http\Controllers\CarController::class, 'view'])->name('showCar');
@@ -31,14 +32,17 @@ Route::post('/updateCar', [App\Http\Controllers\CarController::class, 'update'])
 Route::get('/clientView', [App\Http\Controllers\CarController::class, 'clientView'])->name('clientView');
 Route::get('/carDetail/{id}', [App\Http\Controllers\CarController::class, 'viewCarDetail'])->name('car.detail');
 
+//myCar route
 Route::post('/addMyCar', [App\Http\Controllers\MyCarController::class, 'add'])->name('add.to.car');
 Route::get('/myCar', [App\Http\Controllers\MyCarController::class, 'viewMyCar'])->name('my.car');
 Route::get('/viewMyCar', [App\Http\Controllers\MyCarController::class, 'viewMyCar'])->name('view.myCart'); 
 Route::get('/deleteMyCar/{id}', [App\Http\Controllers\MyCarController::class, 'delete'])->name('deleteMyCar');
 
+//myorder route
 Route::post('/createorder', [App\Http\Controllers\OrderController::class, 'add'])->name('create.order');
 Route::get('/myorder', [App\Http\Controllers\OrderController::class, 'viewOrder'])->name('my.order');
 
+//paypal route
 Route::post('/paypal', [App\Http\Controllers\PaymentController::class, 'payWithpaypal'])->name('paypal');
 Route::get('/status', [App\Http\Controllers\PaymentController::class, 'getPaymentStatus'])->name('status');
 
