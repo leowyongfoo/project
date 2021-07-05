bfa878b52e7cdbcc739225e1b4e15f30f11af871
leowyongfoo <jacksonleow6@gmail.com>
2021-07-05 20:17:00 +0800

    update 5/7

--- carRent/resources/views/layouts/app.blade.php
+++ carRent/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/ico" href="images/car.jpg">
+   
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
